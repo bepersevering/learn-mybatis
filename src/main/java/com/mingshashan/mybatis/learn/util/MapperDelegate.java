942b99fdffc2fd5311ba326b5aec3d3f6818b9e2
mingshashan <mingshashan@gmail.com>
2023-02-01 22:53:53 +0800

    update get mapper method

--- src/main/java/com/mingshashan/mybatis/learn/util/MapperDelegate.java
+++ src/main/java/com/mingshashan/mybatis/learn/util/MapperDelegate.java
@@ -15,8 +15,9 @@ public class MapperDelegate<T> {
     private SqlSessionFactory sqlSessionFactory;
 
     public Object getMapper() {
-        return configuration.getMapperRegistry()
-                .getMapper(mapperClass, sqlSessionFactory.openSession(true));
+        return sqlSessionFactory.openSession().getMapper(mapperClass);
+//        return configuration.getMapperRegistry()
+//                .getMapper(mapperClass, sqlSessionFactory.openSession(true));
     }
 
     public Class getMapperClass() {
